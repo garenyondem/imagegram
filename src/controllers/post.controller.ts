38f9761e9223e587c31feeb1eeb71e501f8fb235
Garen Yondem <gareny@gmail.com>
2021-11-21 20:20:38 +0300

    Add posting endpoint

--- src/controllers/post.controller.ts
+++ src/controllers/post.controller.ts
@@ -1,29 +1,43 @@
 import { Request, Response, NextFunction } from 'express';
 import { Router } from 'express';
-import { PostModel } from '../models/post.model';
+import { IPost, PostModel } from '../models/post.model';
 import { ICommentSchema } from '../schemas/comment.schema';
 
 export const routes = Router();
 
-routes.get('/', async function getPosts(req: Request, res: Response, next: NextFunction): Promise<any> {
+// get all posts
+routes.get('/', async function (req: Request, res: Response, next: NextFunction): Promise<any> {
     const page = req.body.page ? parseInt(req.body.page) : 0;
-
     const posts = await PostModel.getPosts(page);
-
     res.status(200).json(posts);
     next();
 });
 
-routes.get('/:postId', async function getPosts(req: Request, res: Response, next: NextFunction): Promise<any> {
+// get single post
+routes.get('/:postId', async function (req: Request, res: Response, next: NextFunction): Promise<any> {
     const { postId } = req.params;
-
     const post = await PostModel.findById(postId);
-
     res.status(200).json(post);
     next();
 });
 
-routes.get('/:postId/comments', async function getPosts(req: Request, res: Response, next: NextFunction): Promise<any> {
+// post new post
+routes.post('/', async function (req: Request, res: Response, next: NextFunction): Promise<any> {
+    const postParams = req.body as IPost;
+    const userId = req.headers['user-id'] as string;
+    if (!userId) {
+        res.status(500);
+        next();
+        return;
+    }
+    postParams.userId = userId.toObjectId();
+    const post = await PostModel.create(postParams);
+    res.status(200).json(post);
+    next();
+});
+
+// get all comments of post
+routes.get('/:postId/comments', async function (req: Request, res: Response, next: NextFunction): Promise<any> {
     const { postId } = req.params;
 
     const comments = await PostModel.findById(postId).projection('comments');
@@ -32,9 +46,10 @@ routes.get('/:postId/comments', async function getPosts(req: Request, res: Respo
     next();
 });
 
+// get single comment
 routes.get(
     '/:postId/comments/:commentId',
-    async function getPosts(req: Request, res: Response, next: NextFunction): Promise<any> {
+    async function (req: Request, res: Response, next: NextFunction): Promise<any> {
         const { postId, commentId } = req.params;
 
         const comments: ICommentSchema[] = await PostModel.findById(postId).projection('comments');
@@ -48,5 +63,3 @@ routes.get(
 // TODO: delete comment
 // TODO: delete post
 // TODO: upload image
-// TODO: post / comment pagination
-// TODO: mongo implementation
