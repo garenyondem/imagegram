68765f1b38544e5a79150890b2d377f46b9811e7
Garen Yondem <gareny@gmail.com>
2021-11-21 21:24:07 +0300

    Remove func name

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -4,7 +4,7 @@ import { UserModel } from '../models/user.model';
 
 export const routes = Router();
 
-routes.post('/', async function getPosts(req: Request, res: Response, next: NextFunction): Promise<any> {
+routes.post('/', async function (req: Request, res: Response, next: NextFunction): Promise<any> {
     const userParams = req.body;
     const user = await UserModel.create(userParams);
     res.status(200).json(user);
